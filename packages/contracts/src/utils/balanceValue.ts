3c30ba9f804ef29b40fa40849173c99a5de1993e
Frank Jia <jiafrank98@gmail.com>
2022-08-23 21:02:27 -0700

    Docs

--- packages/contracts/src/utils/balanceValue.ts
+++ packages/contracts/src/utils/balanceValue.ts
@@ -1,18 +1,33 @@
 import { BigDecimal } from '@vertex-protocol/utils';
 import { PerpBalanceWithProduct, SpotBalanceWithProduct } from '../common';
 
+/**
+ * Calculates the quote value of a spot balance
+ *
+ * @param balanceWithProduct
+ */
 export function calcSpotBalanceValue(
   balanceWithProduct: SpotBalanceWithProduct,
 ): BigDecimal {
   return balanceWithProduct.amount.multipliedBy(balanceWithProduct.oraclePrice);
 }
 
+/**
+ * Calculates the notional value of a perp balance
+ *
+ * @param balanceWithProduct
+ */
 export function calcPerpBalanceNotionalValue(
   balanceWithProduct: PerpBalanceWithProduct,
 ): BigDecimal {
   return balanceWithProduct.amount.multipliedBy(balanceWithProduct.oraclePrice);
 }
 
+/**
+ * Calculates the true quote value of a perp balance, which is the same as its pnl
+ *
+ * @param balanceWithProduct
+ */
 export function calcPerpBalanceValue(
   balanceWithProduct: PerpBalanceWithProduct,
 ): BigDecimal {
