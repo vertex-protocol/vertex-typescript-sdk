9ffa610f6994f5a3a061dcfa17d9bf7605a9c9ab
Frank Jia <jiafrank98@gmail.com>
2022-09-20 19:17:57 -0700

    To HTTPS endpoint

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -1,5 +1,4 @@
 export const ENGINE_CLIENT_ENDPOINTS = {
   local: 'http://localhost:80/api',
-  testnet:
-    'http://load-appli-2uvu9ksv59sz-1062943129.us-east-2.elb.amazonaws.com/api',
+  testnet: 'https://api.vertexprotocol-backend.com/api',
 };
