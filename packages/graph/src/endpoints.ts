aa5f28c179812fd144f8cc1ba70a64ea54b6f44d
Frank Jia <jiafrank98@gmail.com>
2022-10-05 18:26:04 -0700

    Fix GraphQL endpoint

--- packages/graph/src/endpoints.ts
+++ packages/graph/src/endpoints.ts
@@ -2,5 +2,5 @@
 export const GRAPH_CLIENT_ENDPOINTS = {
   local: 'http://localhost:8000/subgraphs/name/vertex-clearinghouse-subgraph',
   testnet:
-    'https://thegraph.com/hosted-service/subgraph/vertex-protocol/vertex-testnet',
+    'https://api.thegraph.com/subgraphs/name/vertex-protocol/vertex-testnet',
 };
