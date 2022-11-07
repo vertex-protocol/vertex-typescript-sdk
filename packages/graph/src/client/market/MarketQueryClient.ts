573f2f0c39a964d48f6f82bbf728b53fcac466e5
Frank Jia <jiafrank98@gmail.com>
2022-11-06 19:19:28 -0800

    Revert candlestick volume

--- packages/graph/src/client/market/MarketQueryClient.ts
+++ packages/graph/src/client/market/MarketQueryClient.ts
@@ -47,34 +47,22 @@ export class MarketQueryClient extends BaseVertexGraphClient {
     params: GetCandlesticksParams,
   ): Promise<GetCandlesticksResponse> {
     const data = await this.graph.CandlesticksQuery({
-      // Volume is cumulative, so we need an additional datapoint
-      limit: (params.limit ?? 100) + 1,
+      limit: params.limit ?? 100,
       marketEntityId: getMarketEntityId(params.productId),
       maxTimeExclusive: Math.floor(params.beforeTime ?? nowInSeconds()),
     });
 
     // Reverse the array as we want data in reverse chronological order
     const reverseChronologicalCandlesticks = data.candlesticks.reverse();
-    return reverseChronologicalCandlesticks.map(
-      (snapshot, idx): Candlestick => {
-        const prevSnapshot =
-          idx < reverseChronologicalCandlesticks.length
-            ? reverseChronologicalCandlesticks[idx + 1]
-            : undefined;
-        const cumulativeVolume = toBigDecimal(snapshot.volumeQuote);
-        const volume = prevSnapshot
-          ? cumulativeVolume.minus(prevSnapshot.volumeQuote)
-          : cumulativeVolume;
-
-        return {
-          close: fromX18(snapshot.closeX18).toNumber(),
-          high: fromX18(snapshot.highX18).toNumber(),
-          low: fromX18(snapshot.lowX18).toNumber(),
-          open: fromX18(snapshot.openX18).toNumber(),
-          time: toBigDecimal(snapshot.time).toNumber(),
-          volume: volume.toNumber(),
-        };
-      },
-    );
+    return reverseChronologicalCandlesticks.map((snapshot): Candlestick => {
+      return {
+        close: fromX18(snapshot.closeX18).toNumber(),
+        high: fromX18(snapshot.highX18).toNumber(),
+        low: fromX18(snapshot.lowX18).toNumber(),
+        open: fromX18(snapshot.openX18).toNumber(),
+        time: toBigDecimal(snapshot.time).toNumber(),
+        volume: toBigDecimal(snapshot.volumeQuote).toNumber(),
+      };
+    });
   }
 }
