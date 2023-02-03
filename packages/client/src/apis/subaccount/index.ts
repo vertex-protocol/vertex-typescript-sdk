d18977a97d31da5370f61907993545e41bb8320a
yruej301 <jeurymejia301@gmail.com>
2023-02-03 18:32:20 -0500

    add missing export

--- packages/client/src/apis/subaccount/index.ts
+++ packages/client/src/apis/subaccount/index.ts
@@ -2,6 +2,8 @@ import { Mixin } from 'ts-mixer';
 import { SubaccountExecuteAPI } from './SubaccountExecuteAPI';
 import { SubaccountQueryAPI } from './SubaccountQueryAPI';
 
+export * from './queryTypes';
+
 export class SubaccountAPI extends Mixin(
   SubaccountExecuteAPI,
   SubaccountQueryAPI,
