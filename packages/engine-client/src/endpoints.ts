895d7304d4875e77c068101131f0a79c749ec4bd
Frank Jia <jiafrank98@gmail.com>
2023-02-06 11:14:52 -0800

    Switch to test endpoints

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -1,5 +1,6 @@
 // Known endpoints for offchain engine
 export const ENGINE_CLIENT_ENDPOINTS = {
   local: 'http://localhost:80/api',
+  // TODO: Switch back
   testnet: 'https://test.vertexprotocol-backend.com',
 };
