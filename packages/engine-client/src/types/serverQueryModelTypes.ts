5666b044e645dc3a7f15ec6a68fcdadd954f1216
Frank Jia <jiafrank98@gmail.com>
2023-03-06 12:07:07 -0800

    Add min size to queries

--- packages/engine-client/src/types/serverQueryModelTypes.ts
+++ packages/engine-client/src/types/serverQueryModelTypes.ts
@@ -1,79 +1,78 @@
-import { BigNumberish } from 'ethers';
-
 export interface EngineServerSpotBalanceData {
-  amount: BigNumberish;
-  last_cumulative_multiplier_x18: BigNumberish;
+  amount: string;
+  last_cumulative_multiplier_x18: string;
 }
 
 export interface EngineServerSpotLpBalanceData {
-  amount: BigNumberish;
+  amount: string;
 }
 
 export interface EngineServerPerpBalanceData {
-  amount: BigNumberish;
-  v_quote_balance: BigNumberish;
-  last_cumulative_funding_x18: BigNumberish;
+  amount: string;
+  v_quote_balance: string;
+  last_cumulative_funding_x18: string;
 }
 
 export interface EngineServerPerpLpBalanceData {
-  amount: BigNumberish;
-  last_cumulative_funding_x18: BigNumberish;
+  amount: string;
+  last_cumulative_funding_x18: string;
 }
 
 export interface EngineServerRisk {
-  long_weight_initial_x18: BigNumberish;
-  short_weight_initial_x18: BigNumberish;
-  long_weight_maintenance_x18: BigNumberish;
-  short_weight_maintenance_x18: BigNumberish;
-  large_position_penalty_x18: BigNumberish;
+  long_weight_initial_x18: string;
+  short_weight_initial_x18: string;
+  long_weight_maintenance_x18: string;
+  short_weight_maintenance_x18: string;
+  large_position_penalty_x18: string;
 }
 
 export interface EngineServerBookInfo {
-  size_increment: BigNumberish;
-  price_increment_x18: BigNumberish;
-  collected_fees: BigNumberish;
-  lp_spread_x18: BigNumberish;
+  size_increment: string;
+  price_increment_x18: string;
+  min_size: string;
+  collected_fees: string;
+  lp_spread_x18: string;
 }
 
 export interface EngineServerSpotConfig {
   token: string;
-  interest_inflection_util_x18: BigNumberish;
-  interest_floor_x18: BigNumberish;
-  interest_small_cap_x18: BigNumberish;
-  interest_large_cap_x18: BigNumberish;
+  interest_inflection_util_x18: string;
+  interest_floor_x18: string;
+  interest_small_cap_x18: string;
+  interest_large_cap_x18: string;
 }
 
 export interface EngineServerSpotState {
-  cumulative_deposits_multiplier_x18: BigNumberish;
-  cumulative_borrows_multiplier_x18: BigNumberish;
-  total_deposits_normalized: BigNumberish;
-  total_borrows_normalized: BigNumberish;
+  cumulative_deposits_multiplier_x18: string;
+  cumulative_borrows_multiplier_x18: string;
+  total_deposits_normalized: string;
+  total_borrows_normalized: string;
 }
 
 export interface EngineServerSpotLpState {
-  supply: BigNumberish;
+  supply: string;
   quote: EngineServerSpotBalanceData;
   base: EngineServerSpotBalanceData;
 }
 
 export interface EngineServerPerpState {
-  cumulative_funding_long_x18: BigNumberish;
-  cumulative_funding_short_x18: BigNumberish;
-  available_settle: BigNumberish;
-  open_interest: BigNumberish;
+  cumulative_funding_long_x18: string;
+  cumulative_funding_short_x18: string;
+  available_settle: string;
+  open_interest: string;
 }
 
 export interface EngineServerPerpLpState {
-  supply: BigNumberish;
-  base: BigNumberish;
-  quote: BigNumberish;
-  last_cumulative_funding_x18: BigNumberish;
-  cumulative_funding_per_lp_x18: BigNumberish;
+  supply: string;
+  base: string;
+  quote: string;
+  last_cumulative_funding_x18: string;
+  cumulative_funding_per_lp_x18: string;
 }
 
 export interface EngineServerSpotProduct {
   product_id: number;
-  oracle_price_x18: BigNumberish;
+  oracle_price_x18: string;
   risk: EngineServerRisk;
   config: EngineServerSpotConfig;
   state: EngineServerSpotState;
@@ -89,8 +88,8 @@ export interface EngineServerSpotBalance {
 
 export interface EngineServerPerpProduct {
   product_id: number;
-  oracle_price_x18: BigNumberish;
-  index_price_x18: BigNumberish;
+  oracle_price_x18: string;
+  index_price_x18: string;
   risk: EngineServerRisk;
   state: EngineServerPerpState;
   lp_state: EngineServerPerpLpState;
