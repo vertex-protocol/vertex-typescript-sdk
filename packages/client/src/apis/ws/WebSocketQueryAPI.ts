160dc4cfc09c126cad120d2cc03b7b516cc89922
yruej301 <jeurymejia301@gmail.com>
2023-03-21 20:16:28 -0400

    add websocket query builder

--- packages/client/src/apis/ws/WebSocketQueryAPI.ts
+++ packages/client/src/apis/ws/WebSocketQueryAPI.ts
@@ -1,3 +1,15 @@
+import {
+  EngineServerQueryRequest,
+  EngineServerQueryRequestByType,
+  EngineServerQueryRequestType,
+} from '@vertex-protocol/engine-client';
 import { BaseVertexAPI } from '../base';
 
-export class WebSocketQueryAPI extends BaseVertexAPI {}
+export class WebSocketQueryAPI extends BaseVertexAPI {
+  public buildQueryMsg<TRequestType extends EngineServerQueryRequestType>(
+    requestType: TRequestType,
+    params: EngineServerQueryRequestByType[TRequestType],
+  ): EngineServerQueryRequest<TRequestType> {
+    return this.context.engineClient.getQueryRequest(requestType, params);
+  }
+}
