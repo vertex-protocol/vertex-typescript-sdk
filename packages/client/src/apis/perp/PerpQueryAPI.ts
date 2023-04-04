dbb1c686e7f3072ac557fdbd7a6a099e5ae748ba
Frank Jia <jiafrank98@gmail.com>
2023-04-04 13:23:19 -0700

    Some minor adjustments to indexer queries

--- packages/client/src/apis/perp/PerpQueryAPI.ts
+++ packages/client/src/apis/perp/PerpQueryAPI.ts
@@ -1,3 +1,12 @@
 import { BaseVertexAPI } from '../base';
+import { GetIndexerPerpPricesParams } from '@vertex-protocol/indexer-client';
 
-export class PerpQueryAPI extends BaseVertexAPI {}
+export class PerpQueryAPI extends BaseVertexAPI {
+  /**
+   * Gets the latest index & mark price for a perp product
+   * @param params
+   */
+  async getPrices(params: GetIndexerPerpPricesParams) {
+    return this.context.indexerClient.getPerpPrices(params);
+  }
+}
