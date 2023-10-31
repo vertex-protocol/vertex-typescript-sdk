fbd5f1eac8470d8713ef965b516a047b84d60c08
yruej301 <114962854+yruej301@users.noreply.github.com>
2023-10-31 14:55:38 -0400

    update PROD endpoints (#142)

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -3,18 +3,18 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 export const ENGINE_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80',
   testnet: 'https://gateway.test.vertexprotocol.com',
-  mainnet: 'https://api.prod.vertexprotocol.com',
+  mainnet: 'https://gateway.prod.vertexprotocol.com',
 };
 
 export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'ws://localhost:80',
   testnet: 'wss://gateway.test.vertexprotocol.com/v1/ws',
-  mainnet: 'wss://api.prod.vertexprotocol.com/ws',
+  mainnet: 'wss://gateway.prod.vertexprotocol.com/v1/ws',
 };
 
 export const ENGINE_WS_SUBSCRIPTION_CLIENT_ENDPOINTS: Record<ChainEnv, string> =
   {
     local: 'ws://localhost:80',
     testnet: 'wss://gateway.test.vertexprotocol.com/v1/subscribe',
-    mainnet: 'wss://api.prod.vertexprotocol.com/subscribe',
+    mainnet: 'wss://gateway.prod.vertexprotocol.com/v1/subscribe',
   };
