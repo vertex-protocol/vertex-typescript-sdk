fe045b6d5f2e0fbe8418fbffb37ef4263025e333
yruej301 <jeurymejia301@gmail.com>
2023-10-31 09:52:34 -0400

    update testnet backend endpoints

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -2,19 +2,19 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 
 export const ENGINE_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80',
-  testnet: 'https://api.test.vertexprotocol.com',
+  testnet: 'https://gateway.test.vertexprotocol.com/v1',
   mainnet: 'https://api.prod.vertexprotocol.com',
 };
 
 export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'ws://localhost:80',
-  testnet: 'wss://api.test.vertexprotocol.com/ws',
+  testnet: 'wss://gateway.test.vertexprotocol.com/v1/ws',
   mainnet: 'wss://api.prod.vertexprotocol.com/ws',
 };
 
 export const ENGINE_WS_SUBSCRIPTION_CLIENT_ENDPOINTS: Record<ChainEnv, string> =
   {
     local: 'ws://localhost:80',
-    testnet: 'wss://api.test.vertexprotocol.com/subscribe',
+    testnet: 'wss://gateway.test.vertexprotocol.com/v1/subscribe',
     mainnet: 'wss://api.prod.vertexprotocol.com/subscribe',
   };
