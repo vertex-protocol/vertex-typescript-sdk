c3b67c7cf261ef005dcaa99ba90d308d91a09095
yruej301 <jeurymejia301@gmail.com>
2023-10-31 14:51:41 -0400

    update PROD endpoints

--- packages/indexer-client/src/endpoints.ts
+++ packages/indexer-client/src/endpoints.ts
@@ -3,5 +3,5 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 export const INDEXER_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:8000/indexer',
   testnet: 'https://archive.test.vertexprotocol.com/v1',
-  mainnet: 'https://api.prod.vertexprotocol.com/indexer',
+  mainnet: 'https://archive.prod.vertexprotocol.com/v1',
 };
--- packages/trigger-client/src/endpoints.ts
+++ packages/trigger-client/src/endpoints.ts
@@ -3,5 +3,5 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 export const TRIGGER_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80/trigger',
   testnet: 'https://trigger.test.vertexprotocol.com/v1',
-  mainnet: 'https://api.prod.vertexprotocol.com/trigger',
+  mainnet: 'https://trigger.prod.vertexprotocol.com/v1',
 };
