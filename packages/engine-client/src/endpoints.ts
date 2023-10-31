2689788c674b6777827de8ff597b233aa30b8d8a
yruej301 <114962854+yruej301@users.noreply.github.com>
2023-10-31 18:26:43 -0400

    chore: v1 engine endpoints

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -2,8 +2,8 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 
 export const ENGINE_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80',
-  testnet: 'https://gateway.test.vertexprotocol.com',
-  mainnet: 'https://gateway.prod.vertexprotocol.com',
+  testnet: 'https://gateway.test.vertexprotocol.com/v1',
+  mainnet: 'https://gateway.prod.vertexprotocol.com/v1',
 };
 
 export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
