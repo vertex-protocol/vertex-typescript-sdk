5bc5ab976943ca4864dc2c133cd91122f4e577c5
yruej301 <jeurymejia301@gmail.com>
2023-11-14 01:38:25 -0500

    swap testnet to arbi sepolia

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -2,19 +2,19 @@ import { ChainEnv } from '@vertex-protocol/contracts';
 
 export const ENGINE_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'http://localhost:80',
-  testnet: 'https://gateway.test.vertexprotocol.com/v1',
+  testnet: 'https://gateway.sepolia-test.vertexprotocol.com/v1',
   mainnet: 'https://gateway.prod.vertexprotocol.com/v1',
 };
 
 export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   local: 'ws://localhost:80',
-  testnet: 'wss://gateway.test.vertexprotocol.com/v1/ws',
+  testnet: 'wss://gateway.sepolia-test.vertexprotocol.com/v1/ws',
   mainnet: 'wss://gateway.prod.vertexprotocol.com/v1/ws',
 };
 
 export const ENGINE_WS_SUBSCRIPTION_CLIENT_ENDPOINTS: Record<ChainEnv, string> =
   {
     local: 'ws://localhost:80',
-    testnet: 'wss://gateway.test.vertexprotocol.com/v1/subscribe',
+    testnet: 'wss://gateway.sepolia-test.vertexprotocol.com/v1/subscribe',
     mainnet: 'wss://gateway.prod.vertexprotocol.com/v1/subscribe',
   };
