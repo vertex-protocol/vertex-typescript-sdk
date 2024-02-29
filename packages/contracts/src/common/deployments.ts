3ea15211c5586aaef0e15cec8b44e37449e4174a
yruej301 <jeurymejia301@gmail.com>
2024-02-28 23:18:28 -0500

    add blast-mainnet deployment

--- packages/contracts/src/common/deployments.ts
+++ packages/contracts/src/common/deployments.ts
@@ -1,10 +1,12 @@
 import ArbitrumOneCoreDeployment from './deployments/core/deployment.arbitrumOne.json';
 import ArbitrumSepoliaCoreDeployment from './deployments/core/deployment.arbitrumSepolia.json';
+import BlastMainnetCoreDeployment from './deployments/core/deployment.blastMainnet.json';
 import BlastSepoliaCoreDeployment from './deployments/core/deployment.blastSepolia.json';
 import LocalCoreDeployment from './deployments/core/deployment.localhost.json';
-import ArbitrumOneLbaDeployment from './deployments/vrtx/deployment.arbitrumOne.json';
 
+import ArbitrumOneLbaDeployment from './deployments/vrtx/deployment.arbitrumOne.json';
 import ArbitrumSepoliaLbaDeployment from './deployments/vrtx/deployment.arbitrumSepolia.json';
+import BlastMainnetLbaDeployment from './deployments/vrtx/deployment.blastMainnet.json';
 import BlastSepoliaLbaDeployment from './deployments/vrtx/deployment.blastSepolia.json';
 import LocalLbaDeployment from './deployments/vrtx/deployment.localhost.json';
 
@@ -32,9 +34,8 @@ export const VERTEX_DEPLOYMENTS: Record<ChainEnv, VertexDeploymentAddresses> = {
     ...ArbitrumOneCoreDeployment,
   },
   blastMainnet: {
-    // TODO
-    ...ArbitrumOneLbaDeployment,
-    ...ArbitrumOneCoreDeployment,
+    ...BlastMainnetLbaDeployment,
+    ...BlastMainnetCoreDeployment,
   },
   local: {
     ...LocalLbaDeployment,
