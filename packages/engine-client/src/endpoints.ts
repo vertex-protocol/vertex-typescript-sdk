28c4585d4a63f6151fc90504064d20626b77b491
yruej301 <114962854+yruej301@users.noreply.github.com>
2024-05-03 14:13:33 -0400

    add mantle mainnet support (#191)

--- packages/engine-client/src/endpoints.ts
+++ packages/engine-client/src/endpoints.ts
@@ -7,6 +7,7 @@ export const ENGINE_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   arbitrum: 'https://gateway.prod.vertexprotocol.com/v1',
   blast: 'https://gateway.blast-prod.vertexprotocol.com/v1',
   mantleTestnet: 'https://gateway.mantle-test.vertexprotocol.com/v1',
+  mantle: 'https://gateway.mantle-prod.vertexprotocol.com/v1',
 };
 
 export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
@@ -16,6 +17,7 @@ export const ENGINE_WS_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   arbitrum: 'wss://gateway.prod.vertexprotocol.com/v1/ws',
   blast: 'wss://gateway.blast-prod.vertexprotocol.com/v1/ws',
   mantleTestnet: 'wss://gateway.mantle-test.vertexprotocol.com/v1/ws',
+  mantle: 'wss://gateway.mantle-prod.vertexprotocol.com/v1/ws',
 };
 
 export const ENGINE_WS_SUBSCRIPTION_CLIENT_ENDPOINTS: Record<ChainEnv, string> =
@@ -27,4 +29,5 @@ export const ENGINE_WS_SUBSCRIPTION_CLIENT_ENDPOINTS: Record<ChainEnv, string> =
     arbitrum: 'wss://gateway.prod.vertexprotocol.com/v1/subscribe',
     blast: 'wss://gateway.blast-prod.vertexprotocol.com/v1/subscribe',
     mantleTestnet: 'wss://gateway.mantle-test.vertexprotocol.com/v1/subscribe',
+    mantle: 'wss://gateway.mantle-prod.vertexprotocol.com/v1/subscribe',
   };
--- packages/trigger-client/src/endpoints.ts
+++ packages/trigger-client/src/endpoints.ts
@@ -7,4 +7,5 @@ export const TRIGGER_CLIENT_ENDPOINTS: Record<ChainEnv, string> = {
   arbitrum: 'https://trigger.prod.vertexprotocol.com/v1',
   blast: 'https://trigger.blast-prod.vertexprotocol.com/v1',
   mantleTestnet: 'https://trigger.mantle-test.vertexprotocol.com/v1',
+  mantle: 'https://trigger.mantle-prod.vertexprotocol.com/v1',
 };
