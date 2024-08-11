8fd3731b23eba5583e15ddd36a636906e0339d7f
Jeffrey Wang <jwang7605@gmail.com>
2024-08-11 14:27:58 -0700

    Added PRIMARY_CHAIN_IDS.

--- packages/react-client/src/types/PrimaryChain.ts
+++ packages/react-client/src/types/PrimaryChain.ts
@@ -24,6 +24,8 @@ export const PRIMARY_CHAINS = [
   sei,
 ] as const;
 
+export const PRIMARY_CHAIN_IDS = PRIMARY_CHAINS.map((chain) => chain.id);
+
 export type PrimaryChain = (typeof PRIMARY_CHAINS)[number];
 
 export type PrimaryChainID = PrimaryChain['id'];
