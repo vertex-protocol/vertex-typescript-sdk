d821616cd5c9d93bdf0207a89ed38699820cdc22
Frank Jia <jiafrank98@gmail.com>
2025-02-13 15:46:48 -0800

    Simplify processing opts in createClientContext

--- packages/client/src/context.ts
+++ packages/client/src/context.ts
@@ -83,150 +83,25 @@ export function createClientContext(
     indexerEndpoint,
     triggerEndpoint,
   } = ((): VertexClientContextOpts => {
-    if (opts === 'arbitrumTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.arbitrumTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.arbitrumTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.arbitrumTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.arbitrumTestnet,
-      };
+    // Custom options
+    if (typeof opts === 'object') {
+      return opts;
     }
-    if (opts === 'arbitrum') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.arbitrum,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.arbitrum,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.arbitrum,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.arbitrum,
-      };
-    }
-    if (opts === 'blastTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.blastTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.blastTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.blastTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.blastTestnet,
-      };
-    }
-    if (opts === 'blast') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.blast,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.blast,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.blast,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.blast,
-      };
-    }
-    if (opts === 'mantleTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.mantleTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.mantleTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.mantleTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.mantleTestnet,
-      };
-    }
-    if (opts === 'mantle') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.mantle,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.mantle,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.mantle,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.mantle,
-      };
-    }
-    if (opts === 'seiTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.seiTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.seiTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.seiTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.seiTestnet,
-      };
-    }
-    if (opts === 'sei') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.sei,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.sei,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.sei,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.sei,
-      };
-    }
-    if (opts === 'baseTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.baseTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.baseTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.baseTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.baseTestnet,
-      };
-    }
-    if (opts === 'base') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.base,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.base,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.base,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.base,
-      };
-    }
-    if (opts === 'sonicTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.sonicTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.sonicTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.sonicTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.sonicTestnet,
-      };
-    }
-    if (opts === 'sonic') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.sonic,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.sonic,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.sonic,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.sonic,
-      };
-    }
-    if (opts === 'beraTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.beraTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.beraTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.beraTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.beraTestnet,
-      };
-    }
-    if (opts === 'abstractTestnet') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.abstractTestnet,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.abstractTestnet,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.abstractTestnet,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.abstractTestnet,
-      };
-    }
-    if (opts === 'abstract') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.abstract,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.abstract,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.abstract,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.abstract,
-      };
-    }
-    if (opts === 'bera') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.bera,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.bera,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.bera,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.bera,
-      };
-    }
-    if (opts === 'local') {
-      return {
-        contractAddresses: VERTEX_DEPLOYMENTS.local,
-        engineEndpoint: ENGINE_CLIENT_ENDPOINTS.local,
-        indexerEndpoint: INDEXER_CLIENT_ENDPOINTS.local,
-        triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS.local,
-      };
-    }
-    return opts;
+
+    const chainEnv = opts;
+    return {
+      contractAddresses: VERTEX_DEPLOYMENTS[chainEnv],
+      engineEndpoint: ENGINE_CLIENT_ENDPOINTS[chainEnv],
+      indexerEndpoint: INDEXER_CLIENT_ENDPOINTS[chainEnv],
+      triggerEndpoint: TRIGGER_CLIENT_ENDPOINTS[chainEnv],
+    };
   })();
   const { signerOrProvider, linkedSigner } = signerOpts;
 
   const validSigner = isSigner(signerOrProvider) ? signerOrProvider : undefined;
 
   return {
-    signerOrProvider: signerOrProvider,
+    signerOrProvider,
     linkedSigner,
     contracts: {
       querier: FQuerier__factory.connect(
