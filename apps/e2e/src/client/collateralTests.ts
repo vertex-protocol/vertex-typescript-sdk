909d41cb53cad3da87f2dc3bb0b953e379f08c48
Frank Jia <jiafrank98@gmail.com>
2025-04-06 12:02:30 -0700

    Separate integration tests

---
+++ apps/e2e/src/client/collateralTests.ts
@@ -0,0 +1,133 @@
+import { createVertexClient, VertexClient } from '@vertex-protocol/client';
+import { getVertexEIP712Values } from '@vertex-protocol/contracts';
+import { toBigInt, toFixedPoint } from '@vertex-protocol/utils';
+import { encodeAbiParameters, encodePacked, parseAbiParameters } from 'viem';
+import { prettyPrint } from '../utils/prettyPrint';
+import { runWithContext } from '../utils/runWithContext';
+import { RunContext } from '../utils/types';
+import { waitForTransaction } from '../utils/waitForTransaction';
+
+async function collateralTests(context: RunContext) {
+  const walletClient = context.getWalletClient();
+  const publicClient = context.publicClient;
+
+  const vertexClient: VertexClient = createVertexClient(context.env.chainEnv, {
+    walletClient,
+    publicClient,
+  });
+
+  const walletClientAddress = walletClient.account.address;
+
+  console.log('Minting tokens');
+  await waitForTransaction(
+    vertexClient.spot._mintMockERC20({
+      // 100 tokens
+      amount: toFixedPoint(100, 6),
+      productId: 0,
+    }),
+    publicClient,
+  );
+
+  console.log('Approving allowance');
+  await waitForTransaction(
+    vertexClient.spot.approveAllowance({
+      amount: toFixedPoint(100, 6),
+      productId: 0,
+    }),
+    publicClient,
+  );
+
+  /*
+  Deposit 50 via regular flow, and 50 with included referral code
+   */
+
+  console.log('Depositing tokens');
+  await waitForTransaction(
+    vertexClient.spot.deposit({
+      subaccountName: 'default',
+      productId: 0,
+      amount: toFixedPoint(50, 6),
+    }),
+    publicClient,
+  );
+
+  console.log('Depositing tokens with referral code');
+  await waitForTransaction(
+    vertexClient.spot.deposit({
+      subaccountName: 'default',
+      productId: 0,
+      amount: toFixedPoint(50, 6),
+      referralCode: 'Blk23MeZU3',
+    }),
+    publicClient,
+  );
+
+  /*
+  Transfer collateral
+   */
+  const transferResult1 = await vertexClient.spot.transferQuote({
+    amount: toFixedPoint(10),
+    subaccountName: 'default',
+    recipientSubaccountName: 'default2',
+  });
+  prettyPrint('Transfer result #1', transferResult1);
+
+  const transferResult2 = await vertexClient.spot.transferQuote({
+    amount: toFixedPoint(10),
+    subaccountName: 'default2',
+    recipientSubaccountName: 'default',
+  });
+  prettyPrint('Transfer result #2', transferResult2);
+
+  /*
+  Withdraw 50 via regular flow, and 50 via slow-mode
+   */
+  console.log('Withdrawing tokens');
+  const withdrawalResult = await vertexClient.spot.withdraw({
+    subaccountName: 'default',
+    productId: 0,
+    amount: toFixedPoint(50, 6),
+  });
+  prettyPrint('Withdrawal result', withdrawalResult);
+
+  console.log('Slow mode withdrawal');
+  // 1. approve 1 USDC for submitting slow-mode tx
+  await waitForTransaction(
+    vertexClient.spot.approveAllowance({
+      amount: toFixedPoint(1, 6),
+      productId: 0,
+    }),
+    publicClient,
+  );
+  // 2. generate withdraw collateral tx
+  const tx = getVertexEIP712Values('withdraw_collateral', {
+    amount: toFixedPoint(50, 6),
+    nonce: await vertexClient.context.engineClient.getTxNonce(),
+    productId: 0,
+    subaccountName: 'default',
+    subaccountOwner: walletClientAddress,
+  });
+  const encodedTx = encodeAbiParameters(
+    parseAbiParameters('bytes32, uint32, uint128, uint64'),
+    [tx.sender, tx.productId, toBigInt(tx.amount), toBigInt(tx.nonce)],
+  );
+  const encodedSlowModeTx = encodePacked(
+    ['uint8', 'bytes'],
+    [
+      // Withdraw collateral enum value
+      2,
+      encodedTx,
+    ],
+  );
+
+  console.log('Submitting slow mode withdrawal');
+  // 3. submit via slow-mode
+  await waitForTransaction(
+    vertexClient.context.contracts.endpoint.write.submitSlowModeTransaction([
+      encodedSlowModeTx,
+    ]),
+    publicClient,
+  );
+}
+
+runWithContext(collateralTests);
---
+++ apps/e2e/src/client/queryTests.ts
@@ -0,0 +1,124 @@
+import { createVertexClient, VertexClient } from '@vertex-protocol/client';
+import { prettyPrint } from '../utils/prettyPrint';
+import { runWithContext } from '../utils/runWithContext';
+import { RunContext } from '../utils/types';
+
+async function queryTests(context: RunContext) {
+  const walletClient = context.getWalletClient();
+  const publicClient = context.publicClient;
+
+  const vertexClient: VertexClient = createVertexClient(context.env.chainEnv, {
+    walletClient,
+    publicClient,
+  });
+
+  const walletClientAddress = walletClient.account.address;
+
+  prettyPrint('Engine time', await vertexClient.context.engineClient.getTime());
+  prettyPrint(
+    'Symbols',
+    await vertexClient.context.engineClient.getSymbols({}),
+  );
+
+  // Fetches state from offchain sequencer
+  prettyPrint(
+    'Engine All Markets',
+    await vertexClient.market.getAllEngineMarkets(),
+  );
+  prettyPrint(
+    'Edge all markets',
+    await vertexClient.market.getAllEngineMarkets(),
+  );
+
+  // Fetches state from Arbitrum
+  prettyPrint(
+    'On-Chain all markets',
+    await vertexClient.market.getAllMarkets(),
+  );
+
+  prettyPrint(
+    'Latest market prices',
+    await vertexClient.market.getLatestMarketPrices({ productIds: [1, 2, 3] }),
+  );
+  prettyPrint(
+    'Market liquidity',
+    await vertexClient.market.getMarketLiquidity({
+      productId: 3,
+      // Per side of the book
+      depth: 5,
+    }),
+  );
+
+  // Subaccount state from engine
+  prettyPrint(
+    'Subaccount state from engine',
+    await vertexClient.subaccount.getEngineSubaccountSummary({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+    }),
+  );
+  // Subaccount state from Arbitrum
+  prettyPrint(
+    'Subaccount state on-chain',
+    await vertexClient.subaccount.getSubaccountSummary({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+    }),
+  );
+
+  prettyPrint(
+    'Isolated positions',
+    await vertexClient.subaccount.getIsolatedPositions({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+    }),
+  );
+
+  prettyPrint(
+    'Subaccount fee rates',
+    await vertexClient.subaccount.getSubaccountFeeRates({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+    }),
+  );
+
+  prettyPrint(
+    'Subaccount linked signer with rate limit',
+    await vertexClient.subaccount.getSubaccountLinkedSignerWithRateLimit({
+      subaccount: {
+        subaccountOwner: walletClientAddress,
+        subaccountName: 'default',
+      },
+    }),
+  );
+
+  prettyPrint(
+    'Referral code',
+    await vertexClient.subaccount.getReferralCode({
+      subaccount: {
+        subaccountOwner: walletClientAddress,
+        subaccountName: 'default',
+      },
+    }),
+  );
+
+  prettyPrint(
+    'Open subaccount orders',
+    await vertexClient.market.getOpenSubaccountOrders({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+      productId: 1,
+    }),
+  );
+
+  prettyPrint(
+    'Open subaccount multi-product orders',
+    await vertexClient.market.getOpenSubaccountMultiProductOrders({
+      subaccountOwner: walletClientAddress,
+      subaccountName: 'default',
+      productIds: [1, 2, 3],
+    }),
+  );
+}
+
+runWithContext(queryTests);
